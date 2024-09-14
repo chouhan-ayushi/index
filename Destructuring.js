41d456f042836688bc927065bc7625228ef6c97e
chouhan-ayushi <160531488+chouhan-ayushi@users.noreply.github.com>
2024-09-14 13:53:10 +0530

    Add files via upload

---
+++ Destructuring.js
@@ -0,0 +1,55 @@
+// const oneArray =[1,2,3,4]
+// const twoArray = [5,0,8]
+//const threeArray =oneArray.concat(twoArray)
+//const threeArray = [oneArray, twoArray]
+//console.log(threeArray);
+
+//spread operator(array ke elements ko spread karna)
+const oneArray =[1,2,3,4]
+const twoArray = [5,0,8]
+const threeArray =[...oneArray, ...twoArray]
+console.log(threeArray);
+
+// jitne bhi parameters aaye unhe array me push karna
+//part -1
+// function manyArguments(){
+//     let args =Array.from(arguments)
+//     let finalArr =args.map(e=>e)
+//     console.log(finalArr);
+    
+// }
+// manyArguments(3,4,5)
+// manyArguments(5,8,9,2)
+
+// function manyArguments(){
+//     let args =Array.from(arguments)
+//     let finalArr =args.map(e=>e*3)
+//     console.log(finalArr);
+    
+// }
+// manyArguments(3,4,5)
+// manyArguments(5,8,9,2)
+
+//part-2
+//rest operator
+function manyArguments(...args){
+    console.log(typeof args);
+    
+    let finalArr = args.map(e=>e)
+    console.log(finalArr);
+    
+}
+manyArguments(3,4,5)
+ manyArguments(5,8,9,2)
+//-----
+ const prev  =["Ayushi","Sakshi"]
+ const newPrev=["Saloni",...prev,"Sonal"]
+ console.log(newPrev);
+ //---
+ const sitename="PWskills"
+ console.log([...sitename]);
+ //---
+function pwskills(...values){
+    return values
+}
+console.log(pwskills("hii","hello"));
