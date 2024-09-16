22e3dc2dbcb537d6cb178ab1ed4771d64c2a8c24
chouhan-ayushi <160531488+chouhan-ayushi@users.noreply.github.com>
2024-09-16 14:47:13 +0530

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,17 @@
+class Product{
+    // properties - variables -data members
+    name;
+    price;
+    rating;
+
+//behavior - functions - member functions
+display(){
+    console.log("displaying the current product");
+    
+}
+
+
+}
+const p=new Product();
+console.log(p);
+p.display();
