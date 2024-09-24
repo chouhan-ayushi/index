25fda415d32cc3c9b1105567cd263c164d259c12
chouhan-ayushi <160531488+chouhan-ayushi@users.noreply.github.com>
2024-09-24 15:02:01 +0530

    Add files via upload

---
+++ AsyncAwait.js
@@ -0,0 +1,45 @@
+// function createPromise(){
+//     return new Promise(function exec(resolve,reject){
+//         //your code goes here
+//         setTimeout(function f(){
+//             console.log("time out 2");
+//             resolve("ayushi");
+//             },300);
+        
+
+//     });
+// }
+// async function consume(){
+
+//     console.log("inside function");
+//     const response= await createPromise();
+//     console.log("response is",response);
+    
+// }
+// consume();
+
+
+// error handling
+
+function createPromise(){
+    return new Promise(function exec(resolve,reject){
+        //your code goes here
+        setTimeout(function f(){
+            console.log("time out 2");
+            reject("ayushi");
+            },300);
+        
+
+    });
+}
+async function consume(){
+try{
+    console.log("inside function");
+    const response= await createPromise();
+    console.log("response is",response);
+}catch(err) {
+    console.log("handled",err);
+    
+} 
+}
+consume();
