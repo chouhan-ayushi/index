78c30563848819bfdc6a137e6b220b40f05ad22c
chouhan-ayushi <160531488+chouhan-ayushi@users.noreply.github.com>
2024-11-10 16:31:48 +0530

    Update databaseBasic.js

--- databaseBasic.js
+++ databaseBasic.js
@@ -1,3 +1,12 @@
 //this classification is done based on how language takes input from user to perform an action
 -Imperative -->JS, java, C++, Ruby etc
 -Declarative--> SQL
+
+
+show database
+Drop database 
+create database
+use
+create table
+insert into
+select
